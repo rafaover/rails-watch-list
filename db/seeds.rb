5ec3b982f177a449aceeca777d707e067a023923
Rafael Moreira <rafamsilva@gmail.com>
2023-03-28 21:23:17 +1100

    dev: seed file updated with api request - It was used the moviedb API, using Lewagon proxy as get url.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'httparty'
+
+response = HTTParty.get('https://tmdb.lewagon.com/movie/top_rated')
+movies_data = JSON.parse(response.body)['results']
+
+movies_data.each do |movie|
+  Movie.create(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
