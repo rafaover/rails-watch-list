08d01e36ed51ab9aef804f8523d6884e093e1ffc
Rafael Moreira <rafamsilva@gmail.com>
2023-03-28 20:38:42 +1100

    validations models

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,6 @@
 class List < ApplicationRecord
-
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
+  
+  validades :name, presence: true, uniqueness: true
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
+
+  validates :title, :overview, presence: true, uniqueness: true
+  
 end
