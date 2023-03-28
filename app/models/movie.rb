9124368ce7fceb9cb334b7b4fb48ebde1ce6a480
Rafael Moreira <rafamsilva@gmail.com>
2023-03-28 20:51:00 +1100

    added associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,9 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks
+  has_many :bookmarks, before_remove: :check_for_bookmark
 
   validates :title, :overview, presence: true, uniqueness: true
-  
+
+  def check_for_bookmark
+    throw :abort if bookmarks.count.positive?
+  end
 end
