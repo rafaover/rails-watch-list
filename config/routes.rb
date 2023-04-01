3e3688651e2e806b9db585cbb3f9bd9c215533e6
Rafael Moreira <rafamsilva@gmail.com>
2023-04-01 11:30:24 +1100

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'lists#index'
-  resources :lists, only: [:index, :show, :create, :new]
+
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
 end
