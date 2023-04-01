13131e4b7fb8d2b3dca2bdd15258b5cf591befe4
Rafael Moreira <rafamsilva@gmail.com>
2023-04-01 13:50:52 +1100

    bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,7 +1,28 @@
 class BookmarksController < ApplicationController
   def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = List.find(params[:list_id])
+    @bookmark.movie = Movie.find(params[:movie_id])
+    if @bookmark.save
+      redirect_to lists_path(@bookmark.list), notice: "Bookmark was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to @bookmark.list, notice: "Bookmark was successfully removed."
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
   end
 end
