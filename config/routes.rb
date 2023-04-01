12dc99de408a839a2ed0c87a1da0d17f8ed17d3c
Rafael Moreira <rafamsilva@gmail.com>
2023-04-01 18:35:56 +1100

    refac routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'lists#index'
+  root to: 'lists#index'
 
   resources :lists, only: [:index, :new, :create, :show] do
     resources :bookmarks, only: [:new, :create]
