10588b1ba1ba3bf4151004c971d6978aa7ca7c38
Rafael Moreira <rafamsilva@gmail.com>
2023-04-02 15:27:19 +1000

    trying to implement AOS -> Incomplete

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,7 @@
 import "@hotwired/turbo-rails";
 import "./controllers";
 import "bootstrap";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
