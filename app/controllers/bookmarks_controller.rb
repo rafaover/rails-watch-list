51051fb7a476b6cb987ff16d6a194b5911bf72fd
Rafael Moreira <rafamsilva@gmail.com>
2023-04-02 14:13:25 +1000

    updates

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,11 +1,11 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create, :destroy]
+
   def new
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
@@ -16,12 +16,17 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id])
+    @bookmark = @list.Bookmark.find(params[:id])
+    # @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to lists_path(@bookmark.list), notice: "Bookmark was successfully removed."
+    redirect_to list_path(@bookmark.list), notice: "Bookmark was successfully removed."
   end
 
   private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
