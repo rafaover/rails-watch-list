3c763c52f1ba9dd9fef3e6b85cff3cb9a556e93e
Rafael Moreira <rafamsilva@gmail.com>
2023-04-04 20:42:41 +1000

    @bookmark in list.show controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,6 +7,7 @@ class ListsController < ApplicationController
   end
 
   def show
+    @bookmark = Bookmark.new
     @list = List.find(params[:id])
     @reviews = @list.reviews
     @review = Review.new
